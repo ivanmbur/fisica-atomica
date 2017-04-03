daedc5d6a8405bf3b133a19004ea19a4c894d928
Ivan <im.burbano10@uniandes.edu.co>
2017-04-03 00:13:12 -0500

    Tarea 7 ahora compila pero hay errores de segmentacion

--- tarea7/tarea7.c
+++ tarea7/tarea7.c
@@ -2,92 +2,80 @@
 #include <math.h>
 #include <stdlib.h>
 
-/*Las funciones que necesita el código*/
-double D2psi(double *u, double psi_0, double Dpsi_0, double *V, double E);
-double *integrador(double *u, double psi_0, double Dpsi_0, double *V, double E, int n);
-double Dpsi(double *V, double E, double epsilon, double minimo, double maximo);
-double *integrador1(double *V, double E);
-double *energias(double *V, double minimo, double maximo, double deltas);
-double refinador(double *V, double epsilon, double minimo, double maximo);
-double *integrador_final(double *V);
+double *integrador_final(double *u, double psi_0, double *V, double epsilon_derivada, double minimo_derivada, double maximo_derivada, double epsilon_energia, double minimo_energia, double maximo_energia, double delta);
 
 int main()
 {
 	/*Se definen las constantes principales*/
 	
 	/*Numero atómico del átomo de dos electrones*/
-	int Z = 2;
+	const int Z = 2;
 
 	/*Numero de pasos en la integración*/
-	int n_pasos = 100000; 
+	const int N_PASOS = 100000; 
 
 	/*Punto de inicio*/
-	float u_0 = 0.00001;
+	const float U_0 = 0.00001;
 
 	/*Punto final*/
-	float u_n = 40;
+	const float U_N = 40;
 
 	/*Numero de veces que se va a iterar el metodo de Hartree*/
-	int n_iteraciones = 10;
+	const int N_ITERACIONES = 10;
 
 	/*Minima derivada inicial de las funciones de onda*/
-	double minimo_derivada = -5;
+	const double MINIMO_DERIVADA = -5;
 
 	/*Maxima derivada inicial de las funciones de onda*/
-	double maximo_derivada = 0;
+	const double MAXIMO_DERIVADA = 0;
 
 	/*Criterio derivadas*/
-	double epsilon_derivada = 0.01;
+	const double EPSILON_DERIVADA = 0.01;
 	
 	/*Minima energia que se considera*/
-	double minimo_energia = -1;
+	const double MINIMO_ENERGIA = -1;
 
 	/*Maxima energia que se considera*/
-	double maximo_energia = -0.3;
+	const double MAXIMO_ENERGIA = -0.3;
 
 	/*Finura de la busqueda de energias*/
-	double delta_energia = 0.0001;
+	const double DELTA_ENERGIA = 0.0001;
 	
 	/*Preción energias*/
-	double epsilon_energia = 0.0000000001;
+	const double EPSILON_ENERGIA = 0.0000000001;
 
 	/*Valor inicial de la funcion de onda*/
-	double psi_0 = 1;
+	const double PSI_0 = 1;
 
 	/*Se discretiza la posición*/
-	double *u = malloc(n_pasos*sizeof(double));
+	double *u = malloc(N_PASOS*sizeof(double));
 	int i;
-	for(i=0;i<n_pasos;i++)
+	for(i=0;i<N_PASOS;i++)
 	{
-		double u[i] = pow(10,log10(u_0) + ((log10(u_n)-log10(u_0))*i/(n_pasos-1)));
+		u[i] = pow(10,log10(U_0) + ((log10(U_N)-log10(U_0))*i/(N_PASOS-1)));
 	} 
 
 	/*Se inicializa el potencial*/
-	double *V = malloc(n_pasos*sizeof(double));
+	double *V = malloc(sizeof(u));
 	int j;
-	for(j=0;j<n_pasos;j++)
+	for(j=0;j<N_PASOS;j++)
 	{
 		V[j] = -2.0*(1.0+((Z-1.0)*exp(-u[j])))/(Z*u[j]);
 	}	
 	
-	/*double *psi = integrador_final(V);*/
-	int m;
-	for(m=0;m<n_pasos;m++)
-	{
-		printf("%f %f\n", u[m], V[m]);
-	} 
+	double *psi = integrador_final(u, PSI_0, V, EPSILON_DERIVADA, MINIMO_DERIVADA, MAXIMO_DERIVADA, EPSILON_ENERGIA, MINIMO_ENERGIA, MAXIMO_ENERGIA, DELTA_ENERGIA); 
 	
 	return 0;
 }
 
 /*Calcula la segunda derivada de la función de onda dado el punto en el que se quiere calcular u, su valor en el punto anterior psi, su derivada en el punto anterior Dpsi, el potencial en el punto V y la energía de la solucion E*/
-double D2psi(u, psi_0, Dpsi_0, V, E)
+double D2psi(double u, double psi, double Dpsi, double V, double E)
 {
-	return ((psi_0*(V-E)) - (2*Dpsi_0/u));
+	return ((psi*(V-E)) - (2*Dpsi/u));
 } 
 
 /*Calcula una solucion a la ecuacion de Schrödinger radial dadas la posiciones u, las condiciones iniciales sobre la función psi y su derivada Dpsi, el potencial V, la energía de la solucion E y el numero de valores que se quiere calcular n*/	
-double *integrador(u, psi_0, Dpsi_0, V, E, n)
+double * integrador(double *u, double psi_0, double Dpsi_0, double *V, double E, int n)
 {
 	double *solucion = malloc(n*sizeof(float));
 	double Dsolucion = Dpsi_0;
@@ -96,7 +84,7 @@ double *integrador(u, psi_0, Dpsi_0, V, E, n)
 	for(k=1;k<n;k++)
 	{	
 		double delta_u = u[k] - u[k-1];
-		double D2solucion = D2psi(u[k], psi[k-1], Dsolucion, V, E);
+		double D2solucion = D2psi(u[k-1], solucion[k-1], Dsolucion, V[k-1], E);
 		Dsolucion = Dsolucion + (D2solucion*delta_u);
 		solucion[k] = solucion[k-1] + (Dsolucion*delta_u);
 	}
@@ -104,12 +92,14 @@ double *integrador(u, psi_0, Dpsi_0, V, E, n)
 }
 
 /*Calcula la primera derivada de la función de onda que la hace continua dadas el potencial V y la energía E, la distancia entre las derivadas a la cual se considera aceptable el resultado epsilon, el minimo valor que se considera minimo y el maximo valor que se considera maximo. El metodo que se utiliza es de biseccion. Vale la pena decir que si epsilon es muy pequeño este metodo no va a converger pues en efecto la solucion tiene un cambio en las derivadas*/
-double Dpsi(V, E, epsilon, minimo, maximo)
+double Dpsi(double *u, double psi_0, double *V, double E, double epsilon, double minimo, double maximo)
 {
 	double inferior = minimo;
 	double superior = maximo;
 	double *resultado = integrador(u, psi_0, (superior + inferior)/2, V, E, 3); 
 	double distancia = sqrt((((resultado[2]-resultado[1])/(u[2] - u[1]))-((resultado[1]-resultado[0])/(u[1] - u[0])))*(((resultado[2]-resultado[1])/(u[2] - u[1]))-((resultado[1]-resultado[0])/(u[1] - u[0])))); 
+	double distancia1;
+	double distancia2;
 	do
 	{
 		double mitad = (superior + inferior)/2;
@@ -117,7 +107,7 @@ double Dpsi(V, E, epsilon, minimo, maximo)
 		distancia1 = sqrt((((resultado[2]-resultado[1])/(u[2] - u[1]))-((resultado[1]-resultado[0])/(u[1] - u[0])))*(((resultado[2]-resultado[1])/(u[2] - u[1]))-((resultado[1]-resultado[0])/(u[1] - u[0]))));
 		resultado = integrador(u, psi_0, (inferior + mitad)/2, V, E, 3); 
 		distancia2 = sqrt((((resultado[2]-resultado[1])/(u[2] - u[1]))-((resultado[1]-resultado[0])/(u[1] - u[0])))*(((resultado[2]-resultado[1])/(u[2] - u[1]))-((resultado[1]-resultado[0])/(u[1] - u[0])))); 
-		if(distancia1<distacia2)
+		if(distancia1<distancia2)
 		{
 			inferior = mitad;
 			distancia = distancia1;
@@ -128,54 +118,64 @@ double Dpsi(V, E, epsilon, minimo, maximo)
 			distancia = distancia2;
 		}
 	}
-	while(distancia>epsilon)
+	while(distancia>epsilon);
+	free(resultado);
 	return (superior + inferior)/2;
 }		
 
 /*Calcula una solucion a la ecuacion de Schrödinger radial dadas el potencial V y la energía de la solucion E*/	
-double *integrador1(V, E)
+double *integrador1(double *u, double psi_0, double *V, double E, double epsilon, double minimo, double maximo)
 {
-	return integrador(u, psi, Dpsi(V, E, epsilon_derivada, minimo_derivada, maximo_derivada), V, E, n_points);  
+	return integrador(u, psi_0, Dpsi(u, psi_0, V, E, epsilon, minimo, maximo), V, E, sizeof(u)/sizeof(double)); 
 }
 
 /*Encuentra energias dado el potencial V, la energia minima a considerar minimo, la maxima maximo y la finura con la cual se van a buscar energias delta*/
-double *energias(V, minimo, maximo, delta)
+double *energias(double *u, double psi_0, double *V, double epsilon_derivada, double minimo_derivada, double maximo_derivada, double minimo_energia, double maximo_energia, double delta)
 {
+	int n = sizeof(u)/sizeof(double);
 	double *encontradas = malloc(50*sizeof(double));
 	int posicion = 0;
-	double pasado = minimo;
-	double *psi_pasado = integrador1(V, pasado);
-	double paso_pasado = psi_pasado[n_pasos - 1] - psi_pasado[n_pasos - 2];
-	for(E=pasado+delta;E<maximo;E = E+delta)
+	double pasado = minimo_energia;
+	double *psi_pasado = integrador1(u, psi_0, V, pasado, epsilon_derivada, minimo_derivada, maximo_derivada);
+	double paso_pasado = psi_pasado[n - 1] - psi_pasado[n - 2];
+	double E;
+	double paso_presente;
+	double *psi_presente;
+	for(E=pasado+delta;E<maximo_energia;E = E+delta)
 	{	
-		double *psi_presente = integrador1(V, E);
-		paso_presente = psi_presente[n_pasos - 1] - psi_presente[n_pasos - 2];
+		psi_presente = integrador1(u, psi_0, V, E, epsilon_derivada, minimo_derivada, maximo_derivada);
+		paso_presente = psi_presente[n - 1] - psi_presente[n - 2];
 		if(paso_presente/sqrt(paso_presente*paso_presente) != paso_pasado/sqrt(paso_pasado*paso_pasado))
 		{
 			encontradas[posicion] = pasado;
-			encontradas[psicion + 1] = presente;
+			encontradas[posicion + 1] = E;
 			posicion = posicion + 2;
 		}
-		pasado = presente;
+		pasado = E;
 		paso_pasado = paso_presente;
 	}
-	return energias;
+	free(psi_pasado);
+	free(psi_presente);
+	return encontradas;
 }
 		
 
 /*Refina la energia dado el potencial V, la precision epsilon, la energia minima a considerar minimo y la maxima maximo*/
-double refinador(V, epsilon, minimo, maximo)
+double refinador(double *u, double psi_0, double *V, double epsilon_derivada, double minimo_derivada, double maximo_derivada, double epsilon_energia, double minimo_energia, double maximo_energia)
 {
-	double superior = maximo;
-	double inferior = minimo;
+	int n = sizeof(u)/sizeof(double);
+	double superior = maximo_energia;
+	double inferior = minimo_energia;
 	double distancia = superior - inferior;
+	double *psi_superior;
+	double *psi_mitad;
 	do
 	{
 		double mitad = (superior + inferior)/2;
-		double *psi_superior = integrador1(V, superior);
-		double *psi_mitad = integrador1(V, mitad); 
-		double paso_superior = psi_superior[n_pasos - 1] - psi_superior[n_pasos - 2];
-		double paso_mitad = psi_mitad[n_pasos - 1] - psi_mitad[n_pasos - 2];
+		psi_superior = integrador1(u, psi_0, V, superior, epsilon_derivada, minimo_derivada, maximo_derivada);
+		psi_mitad = integrador1(u, psi_0, V, mitad, epsilon_derivada, minimo_derivada, maximo_derivada); 
+		double paso_superior = psi_superior[n - 1] - psi_superior[n - 2];
+		double paso_mitad = psi_mitad[n - 1] - psi_mitad[n - 2];
 		
 		if(paso_superior/sqrt(paso_superior*paso_superior) != paso_mitad/sqrt(paso_mitad*paso_mitad))
 		{
@@ -187,15 +187,17 @@ double refinador(V, epsilon, minimo, maximo)
 		}
 		distancia = superior - inferior;
 	}
-	while(distancia > epsilon)
+	while(distancia > epsilon_energia);
+	free(psi_superior);
+	free(psi_mitad);
 	return (inferior + superior)/2;
 }
 
 /*Calcula el estado base dado un potencial V*/
-double *integrador_final(V)
+double *integrador_final(double *u, double psi_0, double *V, double epsilon_derivada, double minimo_derivada, double maximo_derivada, double epsilon_energia, double minimo_energia, double maximo_energia, double delta)
 {
-	double *encontradas = energias(V, minimo_energia, maximo_energia, delta_energia);
-	double E = refinador(V, epsilon_energia, encontradas[0], encontradas[1]);
-	return integrador1(V, E);
+	double *encontradas = energias(u, psi_0, V, epsilon_derivada, minimo_derivada, maximo_derivada, minimo_energia, maximo_energia, delta);
+	double E = refinador(u, psi_0, V, epsilon_derivada, minimo_derivada, maximo_derivada, epsilon_energia, encontradas[0], encontradas[1]);
+	return integrador1(u, psi_0, V, E, epsilon_derivada, minimo_derivada, maximo_derivada);
 }
 
